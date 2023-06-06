31877051fb754057350a65a9098762f071c3e14e
caicancai <2356672992@qq.com>
2023-06-06 21:04:30 +0800

    extendhash

---
+++ src/include/buffer/buffer_pool_manager_instance.h
@@ -0,0 +1,184 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// buffer_pool_manager_instance.h
+//
+// Identification: src/include/buffer/buffer_pool_manager.h
+//
+// Copyright (c) 2015-2021, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <list>
+#include <mutex>  // NOLINT
+#include <unordered_map>
+
+#include "buffer/buffer_pool_manager.h"
+#include "buffer/lru_k_replacer.h"
+#include "common/config.h"
+#include "container/hash/extendible_hash_table.h"
+#include "recovery/log_manager.h"
+#include "storage/disk/disk_manager.h"
+#include "storage/page/page.h"
+
+namespace bustub {
+
+/**
+ * BufferPoolManager reads disk pages to and from its internal buffer pool.
+ */
+class BufferPoolManagerInstance : public BufferPoolManager {
+ public:
+  /**
+   * @brief Creates a new BufferPoolManagerInstance.
+   * @param pool_size the size of the buffer pool
+   * @param disk_manager the disk manager
+   * @param replacer_k the lookback constant k for the LRU-K replacer
+   * @param log_manager the log manager (for testing only: nullptr = disable logging)
+   */
+  BufferPoolManagerInstance(size_t pool_size, DiskManager *disk_manager, size_t replacer_k = LRUK_REPLACER_K,
+                            LogManager *log_manager = nullptr);
+
+  /**
+   * @brief Destroy an existing BufferPoolManagerInstance.
+   */
+  ~BufferPoolManagerInstance() override;
+
+  /** @brief Return the size (number of frames) of the buffer pool. */
+  auto GetPoolSize() -> size_t override { return pool_size_; }
+
+  /** @brief Return the pointer to all the pages in the buffer pool. */
+  auto GetPages() -> Page * { return pages_; }
+
+ protected:
+  /**
+   * TODO(P1): Add implementation
+   *
+   * @brief Fetch the requested page from the buffer pool.
+   *
+   * 1. Search for the page_id in the page table first.
+   * 1.1 If found, return the page immediately.
+   * 1.2 If not found, find a replacement page from either the free list
+   * or the replacer. (NOTE: always find from the free list first)
+   * 2. If the replacement page is dirty, write it back to disk by calling DiskManager::WritePage().
+   * 3. Delete the replacement page from the page table and insert the new page.
+   * 4. Update the new page metadata, read page content from disk and return the page pointer.
+   *
+   * Before returning, remember to "Pin" the frame by calling replacer.SetEvitable(frame_id, false)
+   * so that the replacer wouldn't evict the frame before the bpm "Unpin"s it.
+   * You also need to record the access history of the frame in the replacer for the lru-k algorithm to work.
+   *
+   * @param page_id id of page to be fetched
+   * @return the requested page
+   */
+  auto FetchPgImp(page_id_t page_id) -> Page * override;
+
+  /**
+   * TODO(P1): Add implementation
+   *
+   * @brief Unpin the target page from the buffer pool.
+   *
+   * Decrement the pin count of a page. If the pin count reaches 0, the frame should be evictable by the replacer.
+   * Also set the dirty flag on the page to indicate if the page was modified.
+   *
+   * @param page_id id of page to be unpinned
+   * @param is_dirty true if the page should be marked as dirty, false otherwise
+   * @return False if the page is not in the page table or its pin count is <= 0 before this call, true otherwise
+   */
+  auto UnpinPgImp(page_id_t page_id, bool is_dirty) -> bool override;
+
+  /**
+   * TODO(P1): Add implementation
+   *
+   * @brief Flush the target page to disk.
+   *
+   * Use the DiskManager::WritePage() method to flush a page to disk.
+   * Unset the dirty flag of the page after flushing.
+   *
+   * @param page_id id of page to be flushed, cannot be INVALID_PAGE_ID
+   * @return False if the page could not be found in the page table, true otherwise
+   */
+  auto FlushPgImp(page_id_t page_id) -> bool override;
+
+  /**
+   * TODO(P1): Add implementation
+   *
+   * @brief Create a new page in the buffer pool.
+   * 在缓冲池中创建一个新页
+   * 1. Call AllocatePage() to get the page id for the new page
+   * 2. Pick a replacement page from either the free list or the replacer. (NOTE: always find from the free list first)
+   * 3. If the replacement page is dirty, write it back to disk by calling DiskManager::WritePage().
+   * 4. Delete the replacement page from the page table and insert the new page.
+   * 5. Update the new page metadata, zero out the memory and return the page pointer.
+   *
+   * Same as FetchPage(), remember to pin and record the access of the frame in the replacer.
+   *
+   * @param[out] page_id id of created page
+   * @return nullptr if no new pages could be created, otherwise pointer to new page
+   */
+  auto NewPgImp(page_id_t *page_id) -> Page * override;
+
+  /**
+   * TODO(P1): Add implementation
+   *
+   * @brief Delete a page from the buffer pool.
+   *
+   * 1. Delete the page from the page table.
+   * 2. Remove the frame from the replacer.
+   * 3. Add the frame back to the free list.
+   * 4. Reset the page's metadata (id, memory, dirty flag).
+   * 4. Call DeallocatePage() to imitate freeing the page on disk.
+   *
+   * @param page_id id of page to be deleted
+   * @return False if the page exists but could not be deleted, true if the page didn't exist or deletion succeeded
+   */
+  auto DeletePgImp(page_id_t page_id) -> bool override;
+
+  /**
+   * TODO(P1): Add implementation
+   *
+   * @brief Flush all the pages in the buffer pool to disk.
+   */
+  void FlushAllPgsImp() override;
+
+  /**
+   * @brief Allocate a page on disk. Caller should acquire the latch before calling this function.
+   * @return the id of the allocated page
+   */
+  auto AllocatePage() -> page_id_t;
+
+  /**
+   * @brief Deallocate a page on disk. Caller should acquire the latch before calling this function.
+   * @param page_id id of the page to deallocate
+   */
+  void DeallocatePage(__attribute__((unused)) page_id_t page_id) {
+    // This is a no-nop right now without a more complex data structure to track deallocated pages
+  }
+
+  /** Number of pages in the buffer pool. */
+  const size_t pool_size_;
+  /** The next page id to be allocated  */
+  std::atomic<page_id_t> next_page_id_ = 0;
+  /** Bucket size for the extendible hash table */
+  const size_t bucket_size_ = 4;
+
+  /** Array of buffer pool pages. */
+  Page *pages_;
+  /** Pointer to the disk manager. */
+  DiskManager *disk_manager_ __attribute__((__unused__));
+  /** Pointer to the log manager. */
+  LogManager *log_manager_ __attribute__((__unused__));
+  /** Page table for keeping track of buffer pool pages. 哈希表*/
+  ExtendibleHashTable<page_id_t, frame_id_t> *page_table_;
+  /** Replacer to find unpinned pages for replacement. LRU替换策略*/
+  LRUKReplacer *replacer_;
+  /** List of free frames that don't have any pages on them. 空闲链表*/
+  std::list<frame_id_t> free_list_;
+  /** This latch protects shared data structures. We recommend updating this comment to describe what it protects. */
+  std::mutex latch_;
+
+  // TODO(student): You may add additional private members and helper functions
+};
+}  // namespace bustub
---
+++ src/include/primer/p0_trie.h
@@ -0,0 +1,432 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// p0_trie.h
+//
+// Identification: src/include/primer/p0_trie.h
+//
+// Copyright (c) 2015-2022, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "common/exception.h"
+#include "common/rwlatch.h"
+
+namespace bustub {
+
+/**
+ * TrieNode is a generic container for any node in Trie.
+ */
+class TrieNode {
+ public:
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Construct a new Trie Node object with the given key char.
+   * is_end_ flag should be initialized to false in this constructor.
+   *
+   * @param key_char Key character of this trie node
+   */
+  explicit TrieNode(char key_char) {
+    is_end_ = false;
+    key_char_ = key_char;
+  }
+
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Move constructor for trie node object. The unique pointers stored
+   * in children_ should be moved from other_trie_node to new trie node.
+   *
+   * @param other_trie_node Old trie node.
+   */
+  TrieNode(TrieNode &&other_trie_node) noexcept {
+    children_ = std::move(other_trie_node.children_);
+    is_end_ = other_trie_node.is_end_;
+    key_char_ = other_trie_node.key_char_;
+  }
+
+  /**
+   * @brief Destroy the TrieNode object.
+   */
+  virtual ~TrieNode() = default;
+
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Whether this trie node has a child node with specified key char.
+   *
+   * @param key_char Key char of child node.
+   * @return True if this trie node has a child with given key, false otherwise.
+   */
+  auto HasChild(char key_char) const -> bool { return children_.count(key_char) == 1; }
+
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Whether this trie node has any children at all. This is useful
+   * when implementing 'Remove' functionality.
+   *
+   * @return True if this trie node has any child node, false if it has no child node.
+   */
+  auto HasChildren() const -> bool { return !children_.empty(); }
+
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Whether this trie node is the ending character of a key string.
+   *
+   * @return True if is_end_ flag is true, false if is_end_ is false.
+   */
+  auto IsEndNode() const -> bool { return is_end_; }
+
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Return key char of this trie node.
+   *
+   * @return key_char_ of this trie node.
+   */
+  auto GetKeyChar() const -> char { return key_char_; }
+
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Insert a child node for this trie node into children_ map, given the key char and
+   * unique_ptr of the child node. If specified key_char already exists in children_,
+   * return nullptr. If parameter `child`'s key char is different than parameter
+   * `key_char`, return nullptr.
+   *
+   * Note that parameter `child` is rvalue and should be moved when it is
+   * inserted into children_map.
+   *
+   * The return value is a pointer to unique_ptr because pointer to unique_ptr can access the
+   * underlying data without taking ownership of the unique_ptr. Further, we can set the return
+   * value to nullptr when error occurs.
+   *
+   * @param key Key of child node
+   * @param child Unique pointer created for the child node. This should be added to children_ map.
+   * @return Pointer to unique_ptr of the inserted child node. If insertion fails, return nullptr.
+   */
+  auto InsertChildNode(char key_char, std::unique_ptr<TrieNode> &&child) -> std::unique_ptr<TrieNode> * {
+    if (children_.count(key_char) != 0U) {
+      return nullptr;
+    }
+    if (child->key_char_ != key_char) {
+      return nullptr;
+    }
+    children_[key_char] = std::move(child);
+    return &children_[key_char];
+  }
+
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Get the child node given its key char. If child node for given key char does
+   * not exist, return nullptr.
+   *
+   * @param key Key of child node
+   * @return Pointer to unique_ptr of the child node, nullptr if child
+   *         node does not exist.
+   */
+  auto GetChildNode(char key_char) -> std::unique_ptr<TrieNode> * {
+    if (children_.count(key_char) == 0) {
+      return nullptr;
+    }
+    return &children_[key_char];
+  }
+
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Remove child node from children_ map.
+   * If key_char does not exist in children_, return immediately.
+   *
+   * @param key_char Key char of child node to be removed
+   */
+  void RemoveChildNode(char key_char) {
+    if (children_.count(key_char) == 0) {
+      return;
+    }
+    children_.erase(key_char);
+  }
+
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Set the is_end_ flag to true or false.
+   *
+   * @param is_end Whether this trie node is ending char of a key string
+   */
+  void SetEndNode(bool is_end) { is_end_ = is_end; }
+
+ protected:
+  /** Key character of this trie node */
+  char key_char_;
+  /** whether this node marks the end of a key */
+  bool is_end_{false};
+  /** A map of all child nodes of this trie node, which can be accessed by each
+   * child node's key char. */
+  std::unordered_map<char, std::unique_ptr<TrieNode>> children_;
+};
+
+/**
+ * TrieNodeWithValue is a node that marks the ending of a key, and it can
+ * hold a value of any type T.
+ */
+template <typename T>
+class TrieNodeWithValue : public TrieNode {
+ private:
+  /* Value held by this trie node. */
+  T value_;
+
+ public:
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Construct a new TrieNodeWithValue object from a TrieNode object and specify its value.
+   * This is used when a non-terminal TrieNode is converted to terminal TrieNodeWithValue.
+   *
+   * The children_ map of TrieNode should be moved to the new TrieNodeWithValue object.
+   * Since it contains unique pointers, the first parameter is a rvalue reference.
+   *
+   * You should:
+   * 1) invoke TrieNode's move constructor to move data from TrieNode to
+   * TrieNodeWithValue.
+   * 2) set value_ member variable of this node to parameter `value`.
+   * 3) set is_end_ to true
+   *
+   * @param trieNode TrieNode whose data is to be moved to TrieNodeWithValue
+   * @param value
+   */
+  TrieNodeWithValue(TrieNode &&trieNode, T value) : TrieNode(std::move(trieNode)) {
+    value_ = value;
+    is_end_ = true;
+  }
+
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Construct a new TrieNodeWithValue. This is used when a new terminal node is constructed.
+   *
+   * You should:
+   * 1) Invoke the constructor for TrieNode with the given key_char.
+   * 2) Set value_ for this node.
+   * 3) set is_end_ to true.
+   *
+   * @param key_char Key char of this node
+   * @param value Value of this node
+   */
+  TrieNodeWithValue(char key_char, T value) : TrieNode(key_char) {
+    value_ = value;
+    is_end_ = true;
+  }
+
+  /**
+   * @brief Destroy the Trie Node With Value object
+   */
+  ~TrieNodeWithValue() override = default;
+
+  /**
+   * @brief Get the stored value_.
+   *
+   * @return Value of type T stored in this node
+   */
+  auto GetValue() const -> T { return value_; }
+};
+
+/**
+ * Trie is a concurrent key-value store. Each key is a string and its corresponding
+ * value can be any type.
+ */
+class Trie {
+ private:
+  /* Root node of the trie */
+  std::unique_ptr<TrieNode> root_;
+  /* Read-write lock for the trie */
+  ReaderWriterLatch latch_;
+
+ public:
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Construct a new Trie object. Initialize the root node with '\0'
+   * character.
+   */
+  Trie() { root_ = std::make_unique<TrieNode>(TrieNode('\0')); }
+
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Insert key-value pair into the trie.
+   *
+   * If the key is an empty string, return false immediately.
+   *
+   * If the key already exists, return false. Duplicated keys are not allowed and
+   * you should never overwrite value of an existing key.
+   *
+   * When you reach the ending character of a key:
+   * 1. If TrieNode with this ending character does not exist, create new TrieNodeWithValue
+   * and add it to parent node's children_ map.
+   * 2. If the terminal node is a TrieNode, then convert it into TrieNodeWithValue by
+   * invoking the appropriate constructor.
+   * 3. If it is already a TrieNodeWithValue,
+   * then insertion fails and returns false. Do not overwrite existing data with new data.
+   *
+   * You can quickly check whether a TrieNode pointer holds TrieNode or TrieNodeWithValue
+   * by checking the is_end_ flag. If is_end_ == false, then it points to TrieNode. If
+   * is_end_ == true, it points to TrieNodeWithValue.
+   *
+   * @param key Key used to traverse the trie and find the correct node
+   * @param value Value to be inserted
+   * @return True if insertion succeeds, false if the key already exists
+   */
+  template <typename T>
+  auto Insert(const std::string &key, T value) -> bool {
+    latch_.WLock();
+    std::unique_ptr<TrieNode> *parent;
+    if (key.empty()) {
+      latch_.WUnlock();
+      return false;
+    }
+    auto p = &root_;
+    for (auto key_char : key) {
+      if (!p->get()->HasChild(key_char)) {
+        p->get()->InsertChildNode(key_char, std::make_unique<TrieNode>(key_char));
+      }
+      parent = p;
+      p = p->get()->GetChildNode(key_char);
+    }
+    if (p->get()->IsEndNode()) {
+      latch_.WUnlock();
+      return false;
+    }
+    auto temp_node = std::move(*p);
+    parent->get()->RemoveChildNode(key.back());
+    auto temp = std::make_unique<TrieNodeWithValue<T>>(std::move(*temp_node), value);
+    parent->get()->InsertChildNode(key.back(), std::move(temp));
+    latch_.WUnlock();
+    return true;
+  }
+
+  auto Dfs(const std::string &key, std::unique_ptr<TrieNode> *root, size_t index, bool *success)
+      -> std::unique_ptr<TrieNode> * {
+    if (index == key.size()) {
+      if (!root->get()->IsEndNode()) {
+        *success = false;
+        return nullptr;
+      }
+      if (!root->get()->HasChildren()) {
+        *success = true;
+        return nullptr;
+      }
+      *success = true;
+      root->get()->SetEndNode(false);
+      return root;
+    }
+    std::unique_ptr<TrieNode> *node;
+    if (root->get()->HasChild(key[index])) {
+      node = Dfs(key, root->get()->GetChildNode(key[index]), index + 1, success);
+      if (!*success) {
+        return nullptr;
+      }
+      if (node == nullptr) {
+        root->get()->RemoveChildNode(key[index]);
+        if (!root->get()->HasChildren() && !root->get()->IsEndNode()) {
+          return nullptr;
+        }
+      }
+      return root;
+    }
+    *success = false;
+    return nullptr;
+  }
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Remove key value pair from the trie.
+   * This function should also remove nodes that are no longer part of another
+   * key. If key is empty or not found, return false.
+   *
+   * You should:
+   * 1) Find the terminal node for the given key.
+   * 2) If this terminal node does not have any children, remove it from its
+   * parent's children_ map.
+   * 3) Recursively remove nodes that have no children and are not terminal node
+   * of another key.
+   *
+   * @param key Key used to traverse the trie and find the correct node
+   * @return True if the key exists and is removed, false otherwise
+   */
+  auto Remove(const std::string &key) -> bool {
+    latch_.WLock();
+    bool a = true;
+    bool *success = &a;
+    Dfs(key, &root_, 0, success);
+    latch_.WUnlock();
+    return *success;
+  }
+
+  /**
+   * TODO(P0): Add implementation
+   *
+   * @brief Get the corresponding value of type T given its key.
+   * If key is empty, set success to false.
+   * If key does not exist in trie, set success to false.
+   * If the given type T is not the same as the value type stored in TrieNodeWithValue
+   * (ie. GetValue<int> is called but terminal node holds std::string),
+   * set success to false.
+   *
+   * To check whether the two types are the same, dynamic_cast
+   * the terminal TrieNode to TrieNodeWithValue<T>. If the casted result
+   * is not nullptr, then type T is the correct type.
+   *
+   * @param key Key used to traverse the trie and find the correct node
+   * @param success Whether GetValue is successful or not
+   * @return Value of type T if type matches
+   */
+  template <typename T>
+  auto GetValue(const std::string &key, bool *success) -> T {
+    latch_.RLock();
+    if (key.empty()) {
+      *success = false;
+      latch_.RUnlock();
+      return {};
+    }
+    auto p = &root_;
+    for (auto key_char : key) {
+      if (p->get()->HasChild(key_char)) {
+        p = p->get()->GetChildNode(key_char);
+      } else {
+        latch_.RUnlock();
+        *success = false;
+        return {};
+      }
+    }
+    if (!p->get()->IsEndNode()) {
+      *success = false;
+      latch_.RUnlock();
+      return {};
+    }
+    auto res = dynamic_cast<TrieNodeWithValue<T> *>(p->get());
+    if (res) {
+      *success = true;
+      latch_.RUnlock();
+      return res->GetValue();
+    }
+    *success = false;
+    latch_.RUnlock();
+    return {};
+  }
+};
+}  // namespace bustub
